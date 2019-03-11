2d17fb21cea09808a5a175dc485a374db50177dc
Yurii Maystrenko <yura1994hd@gmail.com>
2019-03-12 00:47:46 +0200

    create saga statistics

--- src/flux-saga/bus/fetch/statistics/saga/workers/fetchStatistics.js
+++ src/flux-saga/bus/fetch/statistics/saga/workers/fetchStatistics.js
@@ -1,4 +1,4 @@
-// import { eventChannel, END } from 'redux-saga';
+import { eventChannel, END } from 'redux-saga';
 
 import { statisticsActions } from '../../actions';
 import { fetchActionsAsync } from '../../../../ui/fetch/saga/asyncActions';
@@ -7,53 +7,58 @@ import { types } from '../../types';
 
 import { Api } from '../../../../../../core/rest-api/index';
 
-import { put, call, select } from 'redux-saga/effects';
+import { put, call, select, take } from 'redux-saga/effects';
 
-// function countdown(secs) {
-//   return eventChannel(emitter => {
-//     const interval = setInterval(() => {
-//       secs -= 1;
-//       if (secs > 0) {
-//         emitter(secs);
-//       } else {
-//         emitter(END);
-//       }
-//     }, 1000);
-//     return () => {
-//       clearInterval(interval);
-//     };
-//   });
-// }
+const fetchInterval = isFetch => {
+  return eventChannel(emitter => {
+    const interval = setInterval(() => {
+      if (isFetch) {
+        emitter(isFetch);
+      } else {
+        emitter(END);
+      }
+    }, 1000);
+    return () => {
+      clearInterval(interval);
+    };
+  });
+};
 
 export function* callFetchStatisticsWorker() {
-  try {
-    yield put(statisticsActions.setFetchStatisticsRequest());
-    yield put(
-      fetchActionsAsync.setFetchStateAsync({
-        isFetch: true,
-        type: types.SET_FETCH_STATISTICS_REQUEST,
-      }),
-    );
+  const intervalChanel = yield call(fetchInterval, true);
+  while (true) {
+    const isFetch = yield take(intervalChanel);
+    if (isFetch) {
+      try {
+        yield put(statisticsActions.setFetchStatisticsRequest());
+        yield put(
+          fetchActionsAsync.setFetchStateAsync({
+            isFetch: true,
+            type: types.SET_FETCH_STATISTICS_REQUEST,
+          }),
+        );
 
-    const token = yield select(state => state.fetch.user.token);
+        const token = yield select(state => state.fetch.user.token);
 
-    const response = yield call(Api.statistics.getStatisticsUsers, token);
-    const data = yield call([response, response.json]);
+        const response = yield call(Api.statistics.getStatisticsUsers, token);
+        const data = yield call([response, response.json]);
 
-    yield put(statisticsActions.setFetchStatisticsSuccess(data));
-  } catch (error) {
-    yield put(statisticsActions.setFetchStatisticsError());
-    yield put(
-      fetchActionsAsync.setFetchEmitErrorAsync({
-        error: error,
-        type: types.SET_FETCH_STATISTICS_REQUEST,
-      }),
-    );
-  } finally {
-    yield put(
-      fetchActionsAsync.setFetchStateAsync({
-        type: types.SET_FETCH_STATISTICS_REQUEST,
-      }),
-    );
+        yield put(statisticsActions.setFetchStatisticsSuccess(data));
+      } catch (error) {
+        yield put(statisticsActions.setFetchStatisticsError());
+        yield put(
+          fetchActionsAsync.setFetchEmitErrorAsync({
+            error: error,
+            type: types.SET_FETCH_STATISTICS_REQUEST,
+          }),
+        );
+      } finally {
+        yield put(
+          fetchActionsAsync.setFetchStateAsync({
+            type: types.SET_FETCH_STATISTICS_REQUEST,
+          }),
+        );
+      }
+    }
   }
 }
